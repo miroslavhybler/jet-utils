a68782ba4434e9d91b1a13e1c6f7161e75a5eaab
Miroslav Hýbler <miroslav@peko-studio.cz>
2023-09-17 18:31:21 +0200

    example-app - kotlinCompilerExtensionVersion fix

--- example-app/build.gradle.kts
+++ example-app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         buildConfig= true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.0"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
