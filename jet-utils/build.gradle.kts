fabaed25166e93dd03bc3bf45222e488fefd492c
Miroslav Hýbler <miroslav@peko-studio.cz>
2023-09-17 18:31:48 +0200

    jet-utils - kotlinCompilerExtensionVersion fix

--- jet-utils/build.gradle.kts
+++ jet-utils/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.0"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
